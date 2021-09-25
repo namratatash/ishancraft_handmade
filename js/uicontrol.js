d3ad6157b2e70e3d22dbe6b098463d16552d2930
prembhaskal <pbhaskal@gmail.com>
2021-09-25 17:45:54 +0530

    refactoring

--- js/uicontrol.js
+++ js/uicontrol.js
@@ -1,18 +1,18 @@
-// MarkActiveMenu puts active class for the give menuId.
+// MarkActiveMenu puts active class for the give menuId. depends on header menu structure.
 function MarkActiveMenu(menuId) {
     $(function () {
         $("#header").load("header.html", function () {
+            console.debug("header was loaded");
 
-            // mark the selected menu.
-            console.log("header was loaded");
+            // reset all menus
             $(".nav-menu").children("li").each(function () {
                 $(this).children("a").each(function () {
                     $(this).attr("class", "");
                 })
             })
-
+            
+            // mark the selected menu.
             elementSelector= ".nav-menu #" + menuId
-
             $(elementSelector).attr("class", "active");
         });
     })
